7befb7eba0e0405e7caf37be117e33d011797efd
Amujo Oluyemi <oluyemiamujo@gmail.com>
2024-12-24 00:41:37 -0800

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Cybersecurity Analyst", "AI Engineer", "Software Engineer"],
+    strings: ["Cybersecurity Analysis", "Cybersecurity Engineering", "AI Engineering", "Software Engineering", "Cloud Computing"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
